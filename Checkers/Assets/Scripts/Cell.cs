78257b6cd7e1bd85532cb8dec3dc28e1307160a8
Epico <saadmusejee@live.com>
2018-05-01 01:31:55 +0100

    Added starting points

---
+++ Checkers/Assets/Scripts/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+	//Declare cell properties
+	public bool isTopOrBottomRow;
+	public enum SpecialPosition {edge, corner}
+	public SpecialPosition specialPosition;
+	public Cell topLeft, topRight, bottomLeft, bottomRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Checkers/Assets/Scripts/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+	//Declate piece properties
+	public enum type { black, white }
+	public type pieceType;
+	public Cell position;
+	public bool isKing = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
